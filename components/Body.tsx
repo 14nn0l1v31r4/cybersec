e9fc8fd69c004f3992886b7a1d047278dab15886
Iann Molendolff <iannmolendolff@gmail.com>
2024-11-15 21:13:34 -0300

    Gradiente de cores 1

--- components/Body.tsx
+++ components/Body.tsx
@@ -4,7 +4,7 @@ import Qrcodesmart from './Qrcodesmart'
 
 const Body = () => {
   return (
-    <div className=" bg-gray-900 w-full min-h-screen">
+    <div className=" bg-gradient-to-r from-green-950 to-black w-full min-h-screen">
         <div className='text-center font-serif font-extrabold text-4xl md:text-6xl pt-16'>
             Links de Dowload
         </div>
