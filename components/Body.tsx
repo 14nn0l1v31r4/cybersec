fd4c372ccd170442df5d45da17bbd5a7f8bc16b2
Iann Molendolff <iannmolendolff@gmail.com>
2024-11-26 22:10:45 -0300

    Logo instagram e Texto de Apresentacao

--- components/Body.tsx
+++ components/Body.tsx
@@ -3,6 +3,7 @@ import Qrcodepc from './Qrcodepc'
 import Qrcodesmart from './Qrcodesmart'
 import Help from './Help'
 import Form from './Form'
+import Footer from './Footer'
 
 const Body = () => {
   return (
@@ -11,6 +12,38 @@ const Body = () => {
             Noob Security
             
         </div>
+        <div className="border rounded-md md:m-6 text-start md:text-lg md:p-3 shadow-md ">
+          
+        Jogos vêm sendo utilizados não somente para entretenimento, mas também aprendizado, como
+        os jogos educacionais. Estes jogos têm como finalidade mediar a aprendizagem, construir
+        conhecimento e estimular habilidades cognitivas, afetivas e motoras (SANTOS, 2020). Os jogos
+        educacionais podem ser eletrônicos ou analógicos, mas ambos desenvolvem o hábito de desafiar os
+        jogadores, para que eles se mantenham motivados a continuar, utilizando para isso, recursos como
+        pontuação, níveis e recompensas, o que supre anecessidade dos seres.
+        humanos por conquistar ou cumprir objetivos (VIANNA, 2013).O presente projeto intitulado, “Jogando e
+        Aprendendo&quot;, tem como objetivo proporcionar um ambiente interativo voltado para o uso de jogos
+        educacionais. <br/> O jogo a ser apresentado chama-se Noob Security, um jogo de RPG (Role-Playing Game)
+        digital que utiliza elementos de jogo, desenvolvidos por alunos do curso de
+        Bacharelado em Sistemas de Informação. Com o objetivo de apoiar a
+        aprendizagem dos assuntos de segurança da informação, foi desenvolvido para ajudar os alunos
+        de graduação da área de tecnologia da informação a aprenderem e exercitarem os conteúdos abordados em sala de aula de uma
+        forma mais dinâmica, atraente e divertida (RANGEL e ROCHA, 2022).
+        O Noob Security foi desenvolvido após ser identificada uma escassez de jogos digitais de
+        caráter educativo para a área de segurança da informação, voltados para alunos de graduação e
+        que abordem temas diversos nessa área. Com as avaliações realizadas até aqui, o jogo se
+        mostrou uma ferramenta com potencial para contribuir com o processo de aprendizagem dos
+        assuntos propostos. <br/>O <a href="https://beinternetawesome.withgoogle.com/pt-br_br/interland/" className='underline'>Interland</a> foi desenvolvido pela Google como
+        parte da iniciativa <span className="font-semibold">Be Internet Awesome</span>, após a identificação de uma necessidade crescente de
+        recursos educativos para ensinar crianças sobre segurança digital e cidadania online. Focado em
+        temas como privacidade, segurança online, respeito digital e a prevenção de comportamentos
+        prejudiciais, o jogo oferece uma abordagem interativa e envolvente para ensinar os conceitos
+        fundamentais de navegação segura na internet. Com base nas avaliações realizadas, o Interland
+        demonstrou ser uma ferramenta eficaz para o aprendizado desses temas, utilizando uma
+        dinâmica de jogo divertida e educativa para engajar crianças e promover um ambiente digital
+        mais seguro e consciente.
+
+        </div>
+
         <div className='font-mono text-lg mt-10 ml-60'>
               Links para Download</div>
         
@@ -24,6 +57,9 @@ const Body = () => {
         </div>
         <div className='mb-1'>
           <Form />
+        </div>
+        <div>
+          <Footer />
         </div>
     </div>
   )
---
+++ components/Footer.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Footer = () => {
+  return (
+    <div className=''>Footer
+    <div className=''>
+
+    <a href="https://www.instagram.com/projetojogandoeaprendendo/">
+
+      <Image 
+      src={'/assets/icon_instagram.png'} 
+      alt={''}
+      width={100}
+      height={100}
+      className=''
+      />
+  
+      </a>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
